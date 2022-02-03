af3cef8903065ea0d78b7235d623990b2ccfc1ef
Ямщикова Евгения <31244230+Kinkandju@users.noreply.github.com>
2022-02-03 21:18:52 +0300

    Update index.js
    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function reverse (n) {
-  
+      module.exports = function reverse(n) {
+        if (n > 0) {
+            // Преобразовываю номер в строку
+            // let string = String(n);
+
+            // Преобразовываю строку в массив
+            // let split = string.split("");
+
+            // Изменяю порядок следования массива
+            // let reverse = split.reverse();
+
+            // Создаю строку из элементов массива
+            // return reverse.join("");
+
+            return String(n).split("").reverse().join("");
+        }
+
+        if (n < 0) {
+            n = -n;
+
+            return String(n).split("").reverse().join("");
+        }
+    }
 }
