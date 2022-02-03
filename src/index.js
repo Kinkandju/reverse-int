67b4f368cef550cf7617d41d88d45ea930c53626
Ямщикова Евгения <31244230+Kinkandju@users.noreply.github.com>
2022-02-03 21:28:30 +0300

    Update index.js
    fix: change function reverse

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-module.exports = function reverse (n) {
-      module.exports = function reverse(n) {
-        if (n > 0) {
-            // Преобразовываю номер в строку
-            // let string = String(n);
+module.exports = function reverse(n) {
+	if (n > 0) {
+		// Преобразовываю номер в строку
+		// let string = String(n);
 
-            // Преобразовываю строку в массив
-            // let split = string.split("");
+		// Преобразовываю строку в массив
+		// let split = string.split("");
 
-            // Изменяю порядок следования массива
-            // let reverse = split.reverse();
+		// Изменяю порядок следования массива
+		// let reverse = split.reverse();
 
-            // Создаю строку из элементов массива
-            // return reverse.join("");
+		// Создаю строку из элементов массива
+		// return reverse.join("");
 
-            return String(n).split("").reverse().join("");
-        }
+		return String(n).split("").reverse().join("");
+	}
 
-        if (n < 0) {
-            n = -n;
+	if (n < 0) {
+		n = -n;
 
-            return String(n).split("").reverse().join("");
-        }
-    }
+		return String(n).split("").reverse().join("");
+	}
+    
+    //Либо сокращенный вариант без условий: Math.abs(n).toString().split("").reverse().join("");
 }
