1a4b6b581f0f53c414f4e3792c8290689ead1259
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-08-17 12:10:25 +0300

    refactor: change the solution

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 module.exports = function reverse(n) {
-	if (n > 0) {
-		// Преобразовываю номер в строку
-		// let string = String(n);
+	const absoluteValue = Math.abs(n);
+	const stringValue = String(absoluteValue);
+	const arrayValue = stringValue.split('');
+	const reverseValue = arrayValue.reverse();
+	const joinValue = reverseValue.join('');
 
-		// Преобразовываю строку в массив
-		// let split = string.split("");
-
-		// Изменяю порядок следования массива
-		// let reverse = split.reverse();
-
-		// Создаю строку из элементов массива
-		// return reverse.join("");
-
-		return String(n).split("").reverse().join("");
-	}
-
-	if (n < 0) {
-		n = -n;
-
-		return String(n).split("").reverse().join("");
-	}
-    
-    //Либо сокращенный вариант без условий: Math.abs(n).toString().split("").reverse().join("");
+	return joinValue;
 }
